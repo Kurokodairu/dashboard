77d0928a6377d48d94bdcb88daf5efdd6ed6ca4b
Jørgen <jsvendsli@gmail.com>
2025-07-01 14:52:18 +0200

    AAAA

--- src/components/TwitchCard.jsx
+++ src/components/TwitchCard.jsx
@@ -128,7 +128,7 @@ const TwitchCard = () => {
     // Build the auth URL with proper encoding
     const authUrl = `https://id.twitch.tv/oauth2/authorize?` +
       `client_id=${clientId}&` +
-      `redirect_uri=${encodeURIComponent(redirectUri)}/auth/callback&` +
+      `redirect_uri=${encodeURIComponent(redirectUri)}&` +
       `response_type=token&` +
       `scope=${encodeURIComponent(scopes)}`
     
